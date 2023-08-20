5ef156fba22b7d02bfada2c5509e817bfc4d7558
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-20 15:19:32 +0300

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,8 +1,10 @@
 const {validateBody, validateFavorite} = require("./validateBody");
 const isValidId = require('./isValidId');
+const authenticate = require('./authenticate');
 
 module.exports = {
     validateBody,
     validateFavorite,
-    isValidId
+    isValidId,
+    authenticate
 }
