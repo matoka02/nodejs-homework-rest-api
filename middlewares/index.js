8da0528c0f94aa1f53a74a79ae066349658f1aee
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-20 14:12:47 +0300

    update handleMongooseError

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,8 @@
 const {validateBody, validateFavorite} = require("./validateBody");
-const handleMongooseError = require('./handleMongooseError');
 const isValidId = require('./isValidId');
 
 module.exports = {
     validateBody,
     validateFavorite,
-    handleMongooseError,
     isValidId
 }
