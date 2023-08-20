d5908be801e98a1419b3e17276352f3a8d04e388
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-20 17:22:36 +0300

    Update HttpError.js

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,8 +1,16 @@
-const HttpError = (status, message) => {
-    const error = new Error(message);
-    error.status = status;
-    // console.log('ERROR', error.status);
-    return error;
-}
+const errorMessageList = {
+  400: "Bad Request",
+  401: "Not authorized",
+  403: "Forbidden",
+  404: "Not found",
+  409: "Conflict",
+};
+
+const HttpError = (status, message = errorMessageList[status]) => {
+  const error = new Error(message);
+  error.status = status;
+  // console.log('ERROR', error.status);
+  return error;
+};
 
 module.exports = HttpError;
