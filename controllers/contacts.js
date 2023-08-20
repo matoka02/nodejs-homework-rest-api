e2e3cce7aa249870567da33cae04d67d697a20ef
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-20 22:43:03 +0300

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,8 +3,12 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res, next) => {
   const {_id: owner} = req.user;
-  console.log('ola',owner);
-  const contacts = await Contact.find({owner}, "-createdAt -updatedAt",);
+
+  const contacts = await Contact.find({owner}, "-createdAt -updatedAt").populate('owner', 'email subscription');
+  // пагинация
+  // const {page = 1, limit = 10} = req.query;
+  // const skip = (page -1) * limit;
+  // const contacts = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit}).populate('owner', 'email subscription');
   res.status(200).json(contacts);
 };
 
