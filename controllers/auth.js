0a3368a6f25d80deac213bce3814b0246f906767
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-22 19:51:47 +0300

    create avatar

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,15 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require('gravatar');
+const path = require("path");
+const fs = require("fs/promises");
 
 const { User } = require("../models/user");
-
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const { SECRET_KEY } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
   // заполнение поля по умолчанию
@@ -21,16 +25,15 @@ const register = async (req, res) => {
   };
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
   res.status(201).json({
     'user': {
       email: newUser.email,
       subscription,
     }
-    // email: newUser.email,
-    // // password: newUser.password,
-    // // password,
-    // subscription,
+    // password: newUser.password,
+    // password,
   });
 };
 
@@ -76,17 +79,30 @@ const getCurrent = async (req, res) => {
   res.json({ email, subscription });
 };
 
-const update = async (req, res) => {
+const updateSubscribtion = async (req, res) => {
   const {id} = req.params;
   console.log('ola',id);
   const user =  await User.findByIdAndUpdate({ _id: id }, req.body, { new: true });
   res.status(200).json(user);
-}
+};
+
+const updateAvatar = async (req, res) => {
+  const {_id} = req.user;
+  const {path: tempUpload, originalname} = req.file;
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join('avatars', filename);
+  await User.findByIdAndUpdate(_id, {avatarURL});
+
+  res.json({avatarURL});
+};
 
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
-  update: ctrlWrapper(update),
+  updateSubscribtion: ctrlWrapper(updateSubscribtion),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
