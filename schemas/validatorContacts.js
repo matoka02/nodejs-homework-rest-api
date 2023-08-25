2c48813995c42b7612876d0a85d8aab44509f90f
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-25 20:32:40 +0300

    fixed messages from Joi

--- schemas/validatorContacts.js
+++ schemas/validatorContacts.js
@@ -1,14 +1,22 @@
 const joi = require('joi');
 
 const addSchema = joi.object({
-	name: joi.string().min(3).required(),
-	email: joi.string().email().required(),
-	phone: joi.string().min(5).required(),
+	name: joi.string().min(3).required().messages({
+    'any.required': 'missing required name field',
+  }),
+	email: joi.string().email().required().messages({
+    'any.required': 'missing required email field',
+  }),
+	phone: joi.string().min(5).required().messages({
+    'any.required': 'missing required phone field',
+  }),
 	favorite: joi.boolean(),
 });
 
 const updateFavoriteSchema = joi.object({
-  favorite: joi.boolean().required(),
+  favorite: joi.boolean().required().messages({
+    'any.required': 'missing field favorite',
+  }),
 });
 
 const schemas = { 
