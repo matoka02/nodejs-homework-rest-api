c805c44b4e236500261e314fc27b350e4270be72
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-06-21 13:33:57 +0300

    update verfication process
    - add nodemailer;
    - change email-address;

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,14 +1,38 @@
-const sgMail = require("@sendgrid/mail");
+// const sgMail = require("@sendgrid/mail");
+// require("dotenv").config();
+
+// const { SENDGRID_API_KEY } = process.env;
+
+// sgMail.setApiKey(SENDGRID_API_KEY);
+
+// const sendEmail = async (data) => {
+//   const email = { ...data, from: "kurama.sama.024@gmail.com" };
+//   await sgMail.send(email).then(()=>console.log('Email sent'));
+//   return true;
+// };
+
+const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const { SENDGRID_API_KEY } = process.env;
+const { GMAIL_SSL_PASSWORD, EMAIL_FROM } = process.env;
 
-sgMail.setApiKey(SENDGRID_API_KEY);
+const nodemailerConfig = {
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: EMAIL_FROM,
+    pass: GMAIL_SSL_PASSWORD,
+  },
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: "kurama.sama.024@gmail.com" };
-  await sgMail.send(email).then(()=>console.log('Email sent'));
+  const email = { ...data, from: EMAIL_FROM };
+  await transport.sendMail(email);
   return true;
 };
+
 
 module.exports = sendEmail;
